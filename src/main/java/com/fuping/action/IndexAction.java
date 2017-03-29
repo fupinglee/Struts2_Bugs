0ba9bb5ab681d4bb5700396076d0517807fba4d4
李浮生 <heroku_app@163.com>
2017-03-29 23:34:29 +0800

    S2-005

---
+++ src/main/java/com/fuping/action/IndexAction.java
@@ -0,0 +1,16 @@
+package com.fuping.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class IndexAction extends ActionSupport{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String execute(){
+		return SUCCESS;
+	}
+}
